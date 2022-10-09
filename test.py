c07f2fa11666e46f1503b91bd7beca76273bb346
Xander <xander.devisch@ugent.be>
2022-10-09 16:22:04 +0200

    wrapper werkende gekregen

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ env = gym.make("CartPole-v0", render_mode='human')
 for i_episode in range(20):
     observation = env.reset()
     for t in range(100):
-      #   env.render()
+        env.render()
         print(observation)
         action = env.action_space.sample()
         observation, reward, done, info, _ = env.step(action)
