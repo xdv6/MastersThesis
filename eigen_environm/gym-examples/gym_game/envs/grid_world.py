075cd4b62c10815edab728cacfc57a99782eaf78
Xander <xander.devisch@ugent.be>
2023-04-12 14:39:02 +0200

    fixed border path problem on new map

--- eigen_environm/gym-examples/gym_game/envs/grid_world.py
+++ eigen_environm/gym-examples/gym_game/envs/grid_world.py
@@ -47,7 +47,7 @@ class GridWorldEnv(gym.Env):
             2: np.array([-1, 0]),
             3: np.array([0, -1]),
             # noop for testing with keyboard
-            4: np.array([0,0])
+            # 4: np.array([0,0])
         }
 
         assert render_mode is None or render_mode in self.metadata["render_modes"]
