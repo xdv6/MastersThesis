3952fbb7748642819d66b8a15f1f94f05d6bda07
Xander <xander.devisch@ugent.be>
2023-04-30 21:23:38 +0200

    parentdirectory ProtoTree in eigen_env geimporteerd

---
+++ ProtoTree-main/dqn_prototree.py
@@ -0,0 +1,22 @@
+from prototree.prototree import ProtoTree
+from util.log import Log
+import subprocess
+import sys
+import importlib.util
+
+from util.args import get_args, save_args, get_optimizer
+from util.data import get_dataloaders
+from util.init import init_tree
+from util.net import get_network, freeze
+from util.visualize import gen_vis
+from util.analyse import *
+from util.save import *
+from prototree.train import train_epoch, train_epoch_kontschieder
+from prototree.test import eval, eval_fidelity
+from prototree.prune import prune
+from prototree.project import project, project_with_class_constraints
+from prototree.upsample import upsample
+
+import torch
+from shutil import copy
+from copy import deepcopy
