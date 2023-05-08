78c3c11b10d8a9153467d50c61430ba6dba288f9
Xander <xander.devisch@ugent.be>
2023-05-08 15:42:05 +0200

    atari boxing refactored

---
+++ dqn_util.py
@@ -0,0 +1,136 @@
+import os
+import gym
+
+import pygame
+import math
+import random
+import numpy as np
+from collections import namedtuple
+from itertools import count
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+import pkg_resources
+import time
+import matplotlib.pyplot as plt
+
+import wandb
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+
+class DQN(nn.Module):
+
+    def __init__(self, h, w, outputs, device, kernel_size, stride, grey_scale=False):
+        super(DQN, self).__init__()
+        if grey_scale:
+            self.conv1 = nn.Conv2d(1, 16, kernel_size=kernel_size, stride=stride)
+        else:
+            self.conv1 = nn.Conv2d(3, 16, kernel_size=kernel_size, stride=stride)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=kernel_size, stride=stride)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=kernel_size, stride=stride)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.device = device
+        # Number of Linear input connections depends on output of conv2d layers
+        # and therefore the input image size, so compute it.
+        def conv2d_size_out(size, kernel_size = kernel_size, stride = stride):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
+        linear_input_size = convw * convh * 32
+        self.head = nn.Linear(linear_input_size, outputs)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = x.to(self.device)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        return self.head(x.view(x.size(0), -1))
+    
+
+steps_done = 0
+def select_action(state, config, policy_net, n_actions, device):
+    global steps_done
+    sample = random.random()
+
+
+    # eps_threshold = config.get("EPS_END") + (config.get("EPS_START") - config.get("EPS_END")) * math.exp(-1. * steps_done / config.get("EPS_DECAY"))
+    eps_threshold = config.get("EPS_END")
+
+    wandb.log({"eps_threshold": eps_threshold})
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[random.randrange(n_actions)]], device=device, dtype=torch.long)
+    
+
+
+
+
+def optimize_model(policy_net, optimizer, memory, config, device, target_net):
+    if len(memory) < config.get("BATCH_SIZE"):
+        return
+    transitions = memory.sample( config.get("BATCH_SIZE"))
+    # Transpose the batch (see http://stackoverflow.com/a/19343/3343043 for detailed explanation).
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.uint8)
+    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the columns of actions taken
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    next_state_values = torch.zeros( config.get("BATCH_SIZE"), device=device)
+    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+    # Compute the expected Q values
+    expected_state_action_values = ((next_state_values * config.get("GAMMA") + reward_batch).unsqueeze(1))
+
+    # Compute Huber loss
+    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
+    wandb.log({"loss": loss})
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    for param in policy_net.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
